8bcd8f66831b74d679230648a0ef783f52b2c16c
M.Zaki Al Akkari <56824972+MrZakiakkari@users.noreply.github.com>
2023-03-26 13:20:42 +0100

    commit final changes

--- test.py
+++ test.py
@@ -13,9 +13,9 @@ def discriminator():
 def test_generator(generator):
     noise = tf.random.normal([1, 100])
     generated_image = generator(noise)
-    assert generated_image.shape == (1, 28, 28, 1)
+    assert generated_image.shape == (1, 128, 128, 3)
 
 def test_discriminator(discriminator):
-    image = tf.random.normal([1, 28, 28, 1])
+    image = tf.random.normal([1, 128, 128, 3])
     decision = discriminator(image)
     assert decision.shape == (1, 1)
